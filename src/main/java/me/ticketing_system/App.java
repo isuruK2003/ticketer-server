b58a71d527e87bfa0e5b66b048610e1667e4d194
isuru <isurukarunaratna01@gmail.com>
2024-12-07 01:08:44 +0530

    Removed redundant code

--- src/main/java/me/ticketing_system/App.java
+++ src/main/java/me/ticketing_system/App.java
@@ -1,10 +1,17 @@
 package me.ticketing_system;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import me.ticketing_system.cli.Cli;
+
 @SpringBootApplication
 public class App {
+	private static Cli cli = new Cli();
+	private static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}

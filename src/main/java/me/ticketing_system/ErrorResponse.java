9d829e8a64ba3e349c04a13a2d2a545f0928c6b3
isuru <isurukarunaratna01@gmail.com>
2024-12-07 01:09:57 +0530

    Converted to a record

--- src/main/java/me/ticketing_system/ErrorResponse.java
+++ src/main/java/me/ticketing_system/ErrorResponse.java
@@ -1,28 +1,8 @@
 package me.ticketing_system;
 
-public class ErrorResponse {
-    private int status;
-    private String message;
+public record ErrorResponse (
+    Integer status,
+    String message
+) {}
 
-    public ErrorResponse(int status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public int getStatus() {
-    	return this.status;
-    }
-
-    public String getMessage() {
-    	return this.message;
-    }
-
-    public void setStatus(int newStatus) {
-    	this.status = newStatus;
-    }
-
-    public void setMessage(String newMessage) {
-    	this.message = newMessage;
-    }
-}
 

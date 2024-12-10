9ecf1055880fda7320fa46cc4d20a1ff5dc1835a
isuru <isurukarunaratna01@gmail.com>
2024-12-11 03:00:16 +0530

    Added validations constraints

---
+++ src/main/java/me/ticketing_system/ValidationConstraintsController.java
@@ -0,0 +1,23 @@
+package me.ticketing_system;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@CrossOrigin
+@RestController
+@RequestMapping("validations")
+public class ValidationConstraintsController {
+    ValidationConstraintService validationConstraintService;
+
+    public ValidationConstraintsController(ValidationConstraintService validationConstraintService) {
+        this.validationConstraintService = validationConstraintService;
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @GetMapping("/all")
+    public Map<String, ValidationConstraint> getValidationConstraints() {
+        return this.validationConstraintService.loadConstraints();
+    }
+}
